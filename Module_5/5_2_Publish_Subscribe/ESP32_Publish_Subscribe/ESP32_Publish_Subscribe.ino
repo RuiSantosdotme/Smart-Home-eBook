12b78dcfff3cd01d2e76c03a79206120e3a90f8a
Rui Santos <ee11077@fe.up.pt>
2022-09-11 17:17:52 +0100

    Smart Home eBook

---
+++ Module_5/5_2_Publish_Subscribe/ESP32_Publish_Subscribe/ESP32_Publish_Subscribe.ino
@@ -0,0 +1,181 @@
+/*********
+  Rui Santos
+  Complete instructions at https://RandomNerdTutorials.com/smart-home-ebook/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+#include <WiFi.h>
+extern "C" {
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+
+#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+
+#define MQTT_HOST IPAddress(XXX, XXX, X, XXX) //MQTT BROKER IP ADDRESS
+/*for example:
+#define MQTT_HOST IPAddress(192, 168, 1, 106)*/
+#define MQTT_PORT 1883
+#define BROKER_USER "REPLACE_WITH_BROKER_USERNAME"
+#define BROKER_PASS "REPLACE_WITH_BROKER_PASSWORD"
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+unsigned long previousMillis = 0;   // Stores last time a message was published
+const long interval = 10000;        // Interval at which to publish values
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+  Serial.printf("[WiFi-event] event: %d\n", event);
+  switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      connectToMqtt();
+      break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      Serial.println("WiFi lost connection");
+      xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+      xTimerStart(wifiReconnectTimer, 0);
+      break;
+  }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  
+  // Subscribe to topic "led" when it connects to the broker
+  uint16_t packetIdSub = mqttClient.subscribe("led", 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub);
+  
+  // Publish on the "test" topic with qos 0
+  mqttClient.publish("test", 0, true, "test 1");
+  Serial.println("Publishing at QoS 0");
+  // Publish on the "test" topic with qos 1
+  uint16_t packetIdPub1 = mqttClient.publish("test", 1, true, "test 2");
+  Serial.print("Publishing at QoS 1, packetId: ");
+  Serial.println(packetIdPub1);
+  // Publish on the "test" topic with qos 2
+  uint16_t packetIdPub2 = mqttClient.publish("test", 2, true, "test 3");
+  Serial.print("Publishing at QoS 2, packetId: ");
+  Serial.println(packetIdPub2);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  // Do whatever you want when you receive a message
+  // Save the message in a variable
+  String receivedMessage;
+  for (int i = 0; i < len; i++) {
+    Serial.println((char)payload[i]);
+    receivedMessage += (char)payload[i];
+  }
+  // Turn the LED on or off accordingly to the message content
+  if (strcmp(topic, "led") == 0) {
+    if (receivedMessage == "true"){
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    if (receivedMessage == "false"){
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+  }
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  pinMode (LED_BUILTIN, OUTPUT);
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCredentials(BROKER_USER, BROKER_PASS);
+
+  connectToWifi();
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+
+  // Publish an MQTT message on topic esp/bme680/temperature
+  uint16_t packetIdPub1 = mqttClient.publish("counter", 1, true, String(random(0,20)).c_str());
+  Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", "counter", packetIdPub1);
+  }
+}
---
+++ Module_5/5_4_Subscribe/ESP8266_Subscribe/ESP8266_Subscribe.ino
@@ -0,0 +1,228 @@
+/*********
+  Rui Santos
+  Complete instructions at https://RandomNerdTutorials.com/smart-home-ebook/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*********/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <Ticker.h>
+#include <AsyncMqttClient.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define WIFI_SSID "REPLACE_WITH_YOUR_SSID"
+#define WIFI_PASSWORD "REPLACE_WITH_YOUR_PASSWORD"
+
+// BROKER
+#define MQTT_HOST IPAddress(XXX, XXX, X, XXX) //MQTT BROKER IP ADDRESS
+/*for example:
+#define MQTT_HOST IPAddress(192, 168, 1, 106)*/
+#define MQTT_PORT 1883
+#define BROKER_USER "REPLACE_WITH_BROKER_USERNAME"
+#define BROKER_PASS "REPLACE_WITH_BROKER_PASSWORD"
+
+//MQTT Topics
+#define MQTT_PUB_TEMP "esp/bme280/temperature"
+#define MQTT_PUB_HUM "esp/bme280/humidity"
+#define MQTT_PUB_PRES "esp/bme280/pressure"
+
+
+//MQTT Subscribe Topics
+#define MQTT_SUB_DIGITAL "esp/digital/#"
+
+// BME280 I2C
+Adafruit_BME280 bme;
+
+// Variables to hold sensor readings
+float temp;
+float hum;
+float pres;
+
+AsyncMqttClient mqttClient;
+Ticker mqttReconnectTimer;
+
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
+Ticker wifiReconnectTimer;
+
+unsigned long previousMillis = 0;   // Stores last time a message was published
+const long interval = 10000;        // Interval at which to publish values
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) {
+  Serial.println("Connected to Wi-Fi.");
+  connectToMqtt();
+}
+
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
+  Serial.println("Disconnected from Wi-Fi.");
+  mqttReconnectTimer.detach(); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+  wifiReconnectTimer.once(2, connectToWifi);
+}
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  //Subscribe to topics
+  // Subscribe to topic MQTT_SUB_DIGITAL when it connects to the broker
+  uint16_t packetIdSub1 = mqttClient.subscribe(MQTT_SUB_DIGITAL, 2);
+  Serial.print("Subscribing at QoS 2, packetId: ");
+  Serial.println(packetIdSub1);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    mqttReconnectTimer.once(2, connectToMqtt);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  // Do whatever you want when you receive a message
+  
+  // Save the message in a variable
+  String receivedMessage;
+  for (int i = 0; i < len; i++) {
+    Serial.println((char)payload[i]);
+    receivedMessage += (char)payload[i];
+  }
+  // Save topic in a String variable
+  String receivedTopic = String(topic);  
+  Serial.print("Received Topic: ");
+  Serial.println(receivedTopic);
+
+  // Check which GPIO we want to control
+  int stringLen = receivedTopic.length();
+  // Get the index of the last slash
+  int lastSlash = receivedTopic.lastIndexOf("/");
+  // Get the GPIO number (it's after the last slash "/")
+  // esp/digital/GPIO
+  String gpio = receivedTopic.substring(lastSlash+1, stringLen);
+  Serial.print("DIGITAL GPIO: ");
+  Serial.println(gpio);
+  Serial.print("STATE: ");
+  Serial.println(receivedMessage);
+
+  // Check if it is DIGITAL
+  if (receivedTopic.indexOf("digital") > 0) {
+    //Set the specified GPIO as output
+    pinMode(gpio.toInt(), OUTPUT);
+    //Control the GPIO
+    if (receivedMessage == "true"){
+      digitalWrite(gpio.toInt(), HIGH);
+    }
+    else{
+      digitalWrite(gpio.toInt(), LOW);
+    }
+  }
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  // Initialize BME280 sensor 
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+
+  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setCredentials(BROKER_USER, BROKER_PASS);
+
+
+  connectToWifi();
+}
+
+void loop() {
+
+  unsigned long currentMillis = millis();
+  // Every X number of seconds (interval = 10 seconds) 
+  // it publishes a new MQTT message
+  if (currentMillis - previousMillis >= interval) {
+    // Save the last time a new reading was published
+    previousMillis = currentMillis;
+
+    // New BME280 sensor readings
+    temp = bme.readTemperature();
+    //temp = 1.8*bme.readTemperature() + 32;
+    hum = bme.readHumidity();
+    pres = bme.readPressure()/100.0F;
+
+    // Publish an MQTT message on topic esp32/BME2800/temperature
+    uint16_t packetIdPub1 = mqttClient.publish(MQTT_PUB_TEMP, 1, true, String(temp).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_TEMP, packetIdPub1);
+    Serial.printf("Message: %.2f \n", temp);
+
+    // Publish an MQTT message on topic esp32/BME2800/humidity
+    uint16_t packetIdPub2 = mqttClient.publish(MQTT_PUB_HUM, 1, true, String(hum).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId %i: ", MQTT_PUB_HUM, packetIdPub2);
+    Serial.printf("Message: %.2f \n", hum);
+
+    // Publish an MQTT message on topic esp32/BME2800/pressure
+    uint16_t packetIdPub3 = mqttClient.publish(MQTT_PUB_PRES, 1, true, String(pres).c_str());                            
+    Serial.printf("Publishing on topic %s at QoS 1, packetId: %i", MQTT_PUB_PRES, packetIdPub3);
+    Serial.printf("Message: %.3f \n", pres);
+  }
+}
